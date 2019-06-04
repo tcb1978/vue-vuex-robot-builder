867dc389f110a5ed6dc1a4af2610cf90121fbbb9
Matthew Eldredge <mattheweldredge@gmail.com>
2019-06-04 14:07:37 -0700

    display home component, display robotbuilder component and get robot parts indexing

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <nav class="nav">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="Build-A-Bot">
+            Build-A-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="main">
+      <!--<HomePage />-->
+      <RobotBuilder />
+    </main>
+  </div>
+</template>
+
+<script>
+// import HomePage from './home/HomePage';
+import RobotBuilder from './build/RobotBuilder';
+
+export default {
+  name: 'app',
+  components: {
+    // HomePage
+    RobotBuilder
+  },
+};
+</script>
+
+<style>
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+.main {
+  background-color: #fff;
+  margin: 0 auto;
+  max-width: 1024px;
+  padding: 30px;
+}
+
+.header {
+  background-color: #999;
+  max-width: 1024px;
+  margin: 0 auto;
+}
+
+.nav-list {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img class="robot" src="../assets/robot-home.png" alt="robot home" aria-hidden="true">
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  min-height: 300px;
+  text-align: center;
+}
+
+.robot {
+  max-height: 300px;
+}
+</style>
