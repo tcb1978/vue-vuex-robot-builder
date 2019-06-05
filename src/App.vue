9975dc1410a640be4fd587f8d7916e965e708906
Matthew Eldredge <mattheweldredge@gmail.com>
2019-06-04 17:33:11 -0700

    conditionally render sale style

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,6 @@ export default {
 </script>
 
 <style>
-
 body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 .home {
   min-height: 300px;
   text-align: center;
